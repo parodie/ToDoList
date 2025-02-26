b99aa4b0b2c74ad4591fc41c6121d685828f1118
parodie <naitdayaaaicha@gmail.com>
2025-02-26 14:55:47 +0000

    added some screens

---
+++ screens/addTaskScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+
+function AddTaskScreen(){
+    return (
+        <View>
+            <Text>add task Screen</Text>
+        </View>
+    );
+}
+
+export default AddTaskScreen;
---
+++ screens/tasksListScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+
+function TaksListScreen(){
+    return (
+        <View>
+            <Text>tasks List Screen</Text>
+        </View>
+    );
+}
+
+export default TaksListScreen;
